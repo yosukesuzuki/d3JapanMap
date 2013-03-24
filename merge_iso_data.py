b8903095025ee366fcc3a22a317c380497ee8e7f
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2013-03-24 18:44:49 +0900

    prefecture meta datas

---
+++ merge_iso_data.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding: UTF-8
+
+import json
+
+f = open('iso3166-2_data_from_wikipedia.txt', 'r')
+
+prefectures = []
+for line in f:
+    cells = line.strip().split('\t')
+    prefecture = None
+    prefecture = {'prefecture':cells[0],'japanese':cells[1],'capital':cells[2],'region':cells[3],'major_island':cells[4],'iso':cells[10]}
+    print prefecture
+    prefectures.append(prefecture)
+
+print len(prefectures)
+
+f = open('jpn_topo.json', 'r')
+jpn_topo = f.read()
+
+result_dic = {'prefectures':prefectures}
+
+result_file = open('jpn_prefecture_meta_data_sort_by_sound.json','w')
+result_file.write(json.dumps(result_dic, encoding='UTF-8'))
